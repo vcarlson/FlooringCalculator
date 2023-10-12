579fe5c30d938695c79e923c263f3863713c9b0b
vcarlson1 <vcarlson1@css.edu>
2023-10-12 11:48:04 -0500

    Committed and pushed

--- app/src/main/java/css3334/flooringcalculator/MainActivity.java
+++ app/src/main/java/css3334/flooringcalculator/MainActivity.java
@@ -4,9 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -29,10 +31,24 @@ public class MainActivity extends AppCompatActivity {
         buttonShowResults.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent resultsActIntent = new Intent(MainActivity.this, ResultsActivity.class);
-                startActivity(resultsActIntent);
+                //Intent resultsActIntent = new Intent(MainActivity.this, ResultsActivity.class);
+                //startActivity(resultsActIntent);
+                calculateArea();
             }
         });
 
-    }
+        }
+    private void calculateArea() {
+        double length = Double.parseDouble(editTextLength.getText().toString());
+        double width = Double.parseDouble((editTextWidth.getText().toString()));
+        double area = length * width;
+
+        Log.i("3334","area = "+area);
+        Intent intent = new Intent(this, ResultsActivity.class);
+        intent.putExtra("length", length);
+        intent.putExtra("width", width);
+        intent.putExtra("area", area);
+
+        startActivity(intent);
+   }
 }
--- app/src/main/java/css3334/flooringcalculator/ResultsActivity.java
+++ app/src/main/java/css3334/flooringcalculator/ResultsActivity.java
@@ -1,8 +1,28 @@
 package css3334.flooringcalculator;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
-public class ResultsActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ResultsActivity extends AppCompatActivity {
 
     private TextView textViewArea;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_results);
+
+        textViewArea = findViewById(R.id.textViewArea);
+
+       double area = getIntent().getDoubleExtra("area",0);
+        Log.i("3334","area in Results = "+area);
+
+
+
+        textViewArea.setText("The area is:" + area);
+    }
 }
